8ff1ac0a877fd9d27b6c99bb12f482fab5460b02
Christopher Lindsay <42160806+clindsa1@users.noreply.github.com>
2019-02-14 14:02:18 -0500

    Update mega-nodesign.ino
    modified to check incoming serial data for operating mode

--- mega-nodesign.ino
+++ mega-nodesign.ino
@@ -1,13 +1,27 @@
-//minimal code to start using sign
+//choose what sign does before begin()
 
 #include "Mega_Nodesign.h"
 
 Mega_Nodesign sign;
 
+byte global_mode = 0; //always check this value
+
 void setup() {
+  Serial.begin(115200); //lets start with fast
+  
+  while (Serial.available() == 0) { ; } //kill time
+  char val = Serial.read();
+  
+  switch (val) {
+    case 1:
+      //first mode
+      global_mode = 1;
+      break;
+  }
+  //checking vs just set mode to val?
+  
   sign.begin(); //sign starts
 
-  // put your setup code here, to run once:
 }
 
 void loop() {
